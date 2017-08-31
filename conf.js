ec6bcff524825f1ae542db583803ccf777a47fd7
Tandon, Parvesh <Parvesh.Tandon@ncr.com>
2017-08-31 12:04:16 +0530

    Set up E2E testing environment for angularjs application using protractor and cucumber.

---
+++ conf.js
@@ -0,0 +1,27 @@
+/*
+Basic configuration to run your cucumber
+feature files and step definitions with protractor.
+**/
+exports.config = {
+
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  baseUrl: 'http://localhost:1337/',
+
+  capabilities: {
+      browserName:'chrome'
+  },
+
+  framework: 'custom',  // set to "custom" instead of cucumber.
+
+  frameworkPath: require.resolve('protractor-cucumber-framework'),  // path relative to the current config file
+
+  specs: [
+    './*.feature'     // Specs here are the cucumber feature files
+  ],
+
+  // cucumber command line options
+  cucumberOpts: {
+    require: ['./step_definitions/*.js']
+  }
+};
